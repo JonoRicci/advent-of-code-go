163f71b61f035eaa848188faef6a26b643284ff1
Jonathan Ricci <jonathan.ricci@xdesign.com>
2023-12-14 17:05:51 +0000

    feat: Add D06

--- 2023/day_06/main.go
+++ 2023/day_06/main.go
@@ -2,8 +2,10 @@
 package main
 
 import (
+	"fmt"
 	"jonoricci/advent-of-code-go/common"
 	"log"
+	"strconv"
 	"strings"
 	"time"
 
@@ -58,17 +60,97 @@ func main() {
 // Part1 ...
 func Part1(input []string) (int, error) {
 	start := time.Now()
-	sum := 0
+
+	// Check input length
+	if len(input) != 2 {
+		return 0, fmt.Errorf("input should only have two lines")
+	}
+	logger.Debugln("First line:", input[0])
+	logger.Debugln("Second line:", input[1])
+
+	// Remove "Time:" from the first line and "Distance:" from the second line
+	timeLine := strings.TrimPrefix(input[0], "Time:")
+	distanceLine := strings.TrimPrefix(input[1], "Distance:")
+
+	// Parse times
+	timeStrings := strings.Fields(timeLine)
+	times := make([]int, len(timeStrings))
+	for i, t := range timeStrings {
+		logger.Debugln("Position:", i, "Time:", t)
+		time, err := strconv.Atoi(t)
+		if err != nil {
+			return 0, err
+		}
+		times[i] = time
+	}
+
+	// Parse distances
+	distanceStrings := strings.Fields(distanceLine)
+	distances := make([]int, len(distanceStrings))
+	for i, d := range distanceStrings {
+		logger.Debugln("Position:", i, "Distance:", d)
+		distance, err := strconv.Atoi(d)
+		if err != nil {
+			return 0, err
+		}
+		distances[i] = distance
+	}
+
+	// Calculate winning strategy
+	totalWays := 1
+	for i := 0; i < len(times); i++ {
+		ways := 0
+		for j := 0; j < times[i]; j++ {
+			if ((times[i] - j) * j) > distances[i] {
+				ways++
+			}
+		}
+		totalWays *= ways
+		logger.Debugln("Ways:", ways)
+		logger.Debugln("Total ways:", totalWays)
+	}
 
 	logger.Infoln("Part 1 took:", time.Since(start))
-	return sum, nil
+	return totalWays, nil
 }
 
 // Part2 ...
 func Part2(input []string) (int, error) {
 	start := time.Now()
-	sum := 0
+
+	// Check input length
+	if len(input) != 2 {
+		return 0, fmt.Errorf("input should only have two lines")
+	}
+
+	// Swap spaces for nothing
+	timeLine := strings.ReplaceAll(input[0], " ", "")
+	distanceLine := strings.ReplaceAll(input[1], " ", "")
+
+	// Remove "Time:" and "Distance:"
+	timeLine = strings.TrimPrefix(timeLine, "Time:")
+	distanceLine = strings.TrimPrefix(distanceLine, "Distance:")
+	logger.Debugln("Real time:", timeLine)
+	logger.Debugln("Real distance:", distanceLine)
+
+	// Convert time and distance to integers
+	timeInt, err := strconv.Atoi(timeLine)
+	if err != nil {
+		return 0, err
+	}
+	distanceInt, err := strconv.Atoi(distanceLine)
+	if err != nil {
+		return 0, err
+	}
+
+	// Calculate the number of ways to win
+	waysToWin := 0
+	for i := 0; i < timeInt; i++ {
+		if ((timeInt - i) * i) > distanceInt {
+			waysToWin++
+		}
+	}
 
 	logger.Infoln("Part 2 took:", time.Since(start))
-	return sum, nil
+	return waysToWin, nil
 }
